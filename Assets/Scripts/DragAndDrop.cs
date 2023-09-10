abb519723e625f0db89a81ac8829a05502051929
SteveNguyen <nam.ngh2211@mail.com>
2023-09-10 13:51:42 +0700

    Init

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private bool _dragging = true;
+
+    private Vector2 _offset;
+
+    public static bool mouseButtonReleased;
+
+    private Vector3 startPos;
+
+    public GameObject target;
+
+    public GameObject vfxSuccess;
+    public GameObject vfxFail;
+
+    public bool needToScale;
+
+    private Vector3 scaleChange = new Vector3(1f, 1f, 1f);
+
+    private void Awake()
+    {
+        startPos = transform.position;
+    }
+
+    private void OnMouseDown()
+    {
+        _offset = GetMousePos() - (Vector2)transform.position;
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!_dragging) return;
+
+        var mousePosition = GetMousePos();
+
+        transform.position = mousePosition - _offset;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null && collision.gameObject == target)
+        {
+            _dragging = false;
+
+            transform.position = target.transform.position;
+            if (needToScale) transform.localScale = scaleChange;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+
+            StartCoroutine(VFXCourutine());
+        }
+    }
+
+
+    private void OnMouseUp()
+    {
+        mouseButtonReleased = true;
+
+        if (_dragging)
+        {
+            transform.position = startPos;
+            GameObject vfx = Instantiate(vfxFail, transform.position, Quaternion.identity);
+            Destroy(vfx, .75f);
+        }
+    }
+
+    private Vector2 GetMousePos()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private IEnumerator VFXCourutine()
+    {
+        GameObject vfx = Instantiate(vfxSuccess, transform.position, Quaternion.identity);
+        Destroy(vfx, 1f);
+
+        yield return new WaitForSeconds(1f);
+
+        GameManager.Instance.levels[GameManager.Instance.GetCurrentIndex()].gameObjects.Remove(gameObject);
+    }
+}
